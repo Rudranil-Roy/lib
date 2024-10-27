26c26bc573496c52d23f8d829cd93dcdcbb80055
Rudranil Roy <rudranilroy2004@gmail.com>
2024-10-28 00:59:23 +0530

    addreader funtionality added

--- libapp/views.py
+++ libapp/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
-from .models import *
+from django.shortcuts import render,redirect
+from .models import Book, Reader
 # Create your views here.
 
 def home(request):
-    return render(request,'libapp/home.html',context={
+    return render(request,'libapp/home1.html',context={
         'Books':Book.objects.all()
     })
+
+def addreader(request):
+
+    if request.method=='POST':
+        reader_id=request.POST.get('id')
+        reader_name=request.POST.get('name')
+        reader_email=request.POST.get('email')
+        reader_no=int(request.POST.get('no'))
+        reader_address=request.POST.get('address')
+
+        reader= Reader(reader_id=reader_id,reader_name=reader_name,email=reader_email,contact_no=reader_no,address=reader_address)
+        reader.save()
+
+        return redirect('addreader')
+
+    return render(request,'libapp/login.html')
